5d6e119287ef48c3ed0463837d50877ab05e47b1
Divvi Naga Venkata Satish <divvinagavenkata.satish69@gmail.com>
2019-04-30 23:27:09 -0700

    Added database configuration code

--- project.py
+++ project.py
@@ -4,3 +4,11 @@ from flask import Flask
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
+
+
+app = Flask(__name__)
+
+engine = create_engine('sqlite:///restaurant.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
