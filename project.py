8e2d228fde8de6e4339ad71fde708a3a90ed27b4
Divvi Naga Venkata Satish <divvinagavenkata.satish69@gmail.com>
2019-05-01 21:42:46 -0700

    removed attributes from render_template for all functions

--- project.py
+++ project.py
@@ -17,7 +17,7 @@ session = DBSession()
 @app.route('/')
 @app.route('/restaurant/')
 def showRestaurants():
-    return render_template('restaurants.html', restaurants = restaurants)
+    return render_template('restaurants.html')
 
 # route for adding new restaurant
 @app.route('/restaurant/new/')
@@ -27,33 +27,33 @@ def newRestaurant():
 # route for editing existing restaurant
 @app.route('/restaurant/<int:restaurant_id>/edit/')
 def editRestaurant(restaurant_id):
-    return render_template('editRestaurant.html', restaurant = restaurant)
+    return render_template('editRestaurant.html')
 
 # route for deleting existing restaurant
 @app.route('/restaurant/<int:restaurant_id>/delete/')
 def deleteRestaurant(restaurant_id):
-    return render_template('deleteRestaurant.html', restaurant = restaurant, restaurants = restaurants)
+    return render_template('deleteRestaurant.html')
 
 # route for viewing restaurant menu's
 @app.route('/restaurant/<int:restaurant_id>/menu/')
 @app.route('/restaurant/<int:restaurant_id>/')
 def showMenu(restaurant_id):
-    return render_template('menu.html', restaurant = restaurant, menu = items)
+    return render_template('menu.html')
 
 # route for adding new restaurant menu's
 @app.route('/restaurant/<int:restaurant_id>/menu/new')
 def newMenuItem(restaurant_id):
-    return render_template('newMenuItem.html', restaurant_id = restaurant_id)
+    return render_template('newMenuItem.html')
 
 # route for adding new restaurant menu's
 @app.route('/restaurant/<int:restaurant_id>/menu/edit')
 def editMenuItem(restaurant_id):
-    return render_template('editMenuItem.html', item = item, restaurant_id = restaurant_id)
+    return render_template('editMenuItem.html')
 
 # route for adding new restaurant menu's
 @app.route('/restaurant/<int:restaurant_id>/menu/delete')
 def deleteMenuItem(restaurant_id):
-    return render_template('deleteMenuItem.html', item = item, restaurant_id = restaurant_id)
+    return render_template('deleteMenuItem.html')
 
 if __name__ == "__main__":
     app.debug = True
