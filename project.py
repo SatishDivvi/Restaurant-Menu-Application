a5a40a93fd9dab5cb0a98ae992fdd2f954f404da
Divvi Naga Venkata Satish <divvinagavenkata.satish69@gmail.com>
2019-05-05 01:52:19 -0700

    Change return statement to redirect

--- project.py
+++ project.py
@@ -134,9 +134,7 @@ def gdisconnect():
         del login_session['username']
         del login_session['email']
         del login_session['picture']
-        response = make_response(json.dumps('Successfully disconnected.'), 200)
-        response.headers['Content-Type'] = 'application/json'
-        return response
+        return redirect(url_for('showLogin'))
     else:
         response = make_response(json.dumps('Failed to revoke token for given user.', 400))
         response.headers['Content-Type'] = 'application/json'
