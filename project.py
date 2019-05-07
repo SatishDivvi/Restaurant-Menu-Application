8dd639268c666fdd96801ad81669af405cfa55ec
Divvi Naga Venkata Satish <divvinagavenkata.satish69@gmail.com>
2019-05-06 21:41:08 -0700

    Store App_ID and App_Secret

--- project.py
+++ project.py
@@ -154,6 +154,14 @@ def fbconnect():
         return response
     access_token = request.data
 
+    app_id = json.loads(open('fb_client_secrets.json', 'r').read())['web']['app_id']
+    app_secret = json.loads(open('fb_client_secrets.json', 'r').read())['web']['app_secret']
+    url = 'https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=%s&client_secret=%s&fb_exchange_token=%s' % (
+        app_id, app_secret, access_token)
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[1]
+    token = result.split(',')[0].split(':')[1].replace('"', '')
+
 # JSON Get Request for Restaurants
 @app.route('/restaurant/JSON')
 def restaurantsJSON():
